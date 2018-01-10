54555e0aba2251e6c792b7eac852e0754f4edf67
Atsushi Tadokoro <tadokoro@gmail.com>
2018-01-10 16:25:04 +0900

    update

--- 05_05_3DNoiseMesh/src/ofApp.h
+++ 05_05_3DNoiseMesh/src/ofApp.h
@@ -3,6 +3,7 @@
 #include "ofMain.h"
 
 class ofApp : public ofBaseApp{
+    
 public:
     void setup();
     void update();
@@ -10,15 +11,16 @@ public:
     
     void keyPressed(int key);
     void keyReleased(int key);
-    void mouseMoved(int x, int y);
+    void mouseMoved(int x, int y );
     void mouseDragged(int x, int y, int button);
     void mousePressed(int x, int y, int button);
     void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
     void windowResized(int w, int h);
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
     
-	ofEasyCam cam; // カメラ
-	ofVboMesh mesh; // 3Dメッシュ
-    int w, h;
+    ofMesh mesh;
+    ofEasyCam cam;
 };
