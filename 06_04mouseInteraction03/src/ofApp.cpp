d5fba40d16375da5fa50b91627799ab702d4cf71
Atsushi Tadokoro <tadokoro@gmail.com>
2018-01-25 07:42:04 +0900

    update

--- 06_04mouseInteraction03/src/ofApp.cpp
+++ 06_04mouseInteraction03/src/ofApp.cpp
@@ -2,83 +2,92 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	ofSetFrameRate(60);
-	ofBackground(0);
-	currentPos = ofVec2f(ofGetWidth() / 2, ofGetHeight() / 2);
-	velocity = ofVec2f(0, 0);
-	stiffness = 0.05;
-	friction = 0.95;
-	mass = 1.0;
+    ofSetFrameRate(60);
+    ofBackground(0);
+    // 現在位置を画面の中心に
+    currentPos = ofVec2f(ofGetWidth() / 2, ofGetHeight() / 2);
+    // 速度、ばね定数、摩擦力、質量を設定
+    velocity = ofVec2f(0, 0);
+    stiffness = 0.05;
+    friction = 0.95;
+    mass = 1.0;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-	toPos = ofVec2f(mouseX, mouseY);
-	ofVec2f force = stiffness * (toPos - currentPos);
-	ofVec2f accel = force / mass;
-	velocity = friction * (velocity + accel);
-	currentPos += velocity;
+    // マウスポインタの位置を目標位置に
+    toPos = ofVec2f(mouseX, mouseY);
+    // フックの法則から物体にかかる力を計算
+    ofVec2f force = stiffness * (toPos - currentPos);
+    // 力と質量から加速度を計算 (f = ma ニュートン力学)
+    ofVec2f accel = force / mass;
+    // 速度、加速度、摩擦力から現在の速度を計算
+    velocity = friction * (velocity + accel);
+    // 速度から位置を計算
+    currentPos += velocity;
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	ofSetColor(255);
-	ofDrawCircle(currentPos, 20);
-	ofSetColor(190);
-	ofDrawLine(currentPos, toPos);
+    // 現在位置に円を描画
+    ofSetColor(255);
+    ofDrawCircle(currentPos, 20);
+    // 円とマウスポインタの間に線を描画 (ばね)
+    ofSetColor(190);
+    ofDrawLine(currentPos, toPos);
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseEntered(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseExited(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+    
 }
--- 06_04mouseInteraction03/src/ofApp.h
+++ 06_04mouseInteraction03/src/ofApp.h
@@ -3,28 +3,28 @@
 #include "ofMain.h"
 
 class ofApp : public ofBaseApp {
-
+    
 public:
-	void setup();
-	void update();
-	void draw();
-
-	void keyPressed(int key);
-	void keyReleased(int key);
-	void mouseMoved(int x, int y);
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void mouseEntered(int x, int y);
-	void mouseExited(int x, int y);
-	void windowResized(int w, int h);
-	void dragEvent(ofDragInfo dragInfo);
-	void gotMessage(ofMessage msg);
-
-	float stiffness;
-	float friction;
-	float mass;
-	ofVec2f currentPos;
-	ofVec2f toPos;
-	ofVec2f velocity;
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y);
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    float stiffness;    // ばね定数(ばねの硬さ)
+    float friction;     // 摩擦力(抵抗)
+    float mass;         // 物体の質量
+    ofVec2f currentPos; // 現在位置
+    ofVec2f toPos;      // 目標位置
+    ofVec2f velocity;   // 移動速度
 };
